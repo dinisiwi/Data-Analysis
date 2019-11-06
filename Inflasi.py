2c0b3200203a558cbb83952c8dcc9515651ca368
dinisiwi <57424014+dinisiwi@users.noreply.github.com>
2019-11-07 01:03:59 +0700

    Update Inflasi.py

--- Inflasi.py
+++ Inflasi.py
@@ -91,3 +91,8 @@ plt.show()
 print(plt.show())
 ![](Image/Grafik.png)
 
+#Hasil analisis deskriptif menunjukkan bahwa :
+#Tingkat inflasi tertinggi terjadi pada November 2005 sebesar 18.38%
+#Tingkat inflasi terendah terjadi pada Desember 2002 sebesar 0%
+#Rata-rata inflasi dari Desember 2002 hingga Juni 2019 adalah sebesar 6.31%
+#Tingkat keragaman inflasi sebesar 0.1094%
